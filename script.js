3a059a89b0e3108fba95696f2a6fc89b896220d9
LHwang01 <LawrenceHwang01@gmail.com>
2022-04-30 15:22:05 -0400

    Added Fruit Visibility Function

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const apple = document.getElementById("apple");
+const grapes = document.getElementById("grapes");
+const orange = document.getElementById("orange");
+const pear = document.getElementById("pear");
+const watermelon = document.getElementById("watermelon");
+
 const button = document.getElementById("button");
 var recognition = new webkitSpeechRecognition();
+
+function clearVisibility() {
+    apple.style = "display: none;";
+    grapes.style = "display: none;";
+    orange.style = "display: none;";
+    pear.style = "display: none;";
+    watermelon.style = "display: none;";
+}
 
 button.addEventListener("click", () => {
     if (button.innerText === "Speak") {
@@ -10,20 +24,27 @@ button.addEventListener("click", () => {
         button.innerText = "Speak";
     }
 
+    clearVisibility();
+
     recognition.onresult = function (event) {
         // console.log(event.results[0][0].transcript);
         let wordSpoken = event.results[0][0].transcript + "";
         wordSpoken = wordSpoken.toLowerCase();
 
         if (wordSpoken == "apple") {
+            apple.style = "display: inline;";
             console.log("apple");
         } else if (wordSpoken == "grapes") {
+            grapes.style = "display: inline";
             console.log("grapes");
-        } else if (wordSpoken == "Orange") {
+        } else if (wordSpoken == "orange") {
+            orange.style = "display: inline";
             console.log("orange");
         } else if (wordSpoken == "pear" || wordSpoken == "pair") {
+            pear. style = "display: inline";
             console.log("pear");
         } else if (wordSpoken == "watermelon") {
+            watermelon.style = "display: inline";
             console.log("watermelon")
         } else {
             console.log("unknown");
